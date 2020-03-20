b56bedfee55c5a66dc59d80d21d99cb9f386cce5
peterpeterparker <david.dalbusco@outlook.com>
2020-03-20 08:05:48 +0100

    feat: add canonical urls plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,12 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
+      resolve: `gatsby-plugin-react-helmet-canonical-urls`,
+      options: {
+        siteUrl: `https://happy-apps.org`,
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
