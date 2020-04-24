b0572d9c3b74c3c0b1f5473b3d86126b954f9e95
peterpeterparker <david.dalbusco@outlook.com>
2020-04-24 16:20:40 +0200

    feat: about footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -12,7 +12,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 class Footer extends React.Component {
   render() {
     return (
-      <footer className={styles.footer}>
+      <footer
+        className={`${styles.footer} ${
+          this.props.pageName === "about" ? styles.about : ""
+        }`}
+      >
         {this.props.pageName === "home" ? (
           <div className={styles.bike}>
             <Img fluid={this.props.data.happyBikeImage.childImageSharp.fluid} />
@@ -39,31 +43,56 @@ class Footer extends React.Component {
             "--left": "50%",
           }}
         >
-          <Link
-            to={
-              this.props.pageName === "portfolio"
-                ? `/${this.props.langKey}/about`
-                : `/${this.props.langKey}/portfolio`
-            }
-          >
-            <div className={styles.link}>
-              <span>
-                <FormattedMessage
-                  id={
-                    this.props.pageName === "portfolio"
-                      ? "menu.item.we.are.happy"
-                      : "check.out.appetizers"
-                  }
-                  values={{
-                    strong: (...chunks) => <strong>{chunks}</strong>,
-                  }}
-                />
-              </span>
-              <FontAwesomeIcon icon={["fas", "arrow-right"]} size="2x" />
-            </div>
-          </Link>
+          {this.renderLink()}
+          {this.renderCustomFuture()}
         </Bubble>
       </footer>
+    )
+  }
+
+  renderLink() {
+    if (this.props.pageName !== "portfolio" && this.props.pageName !== "home") {
+      return undefined
+    }
+
+    return (
+      <Link
+        to={
+          this.props.pageName === "portfolio"
+            ? `/${this.props.langKey}/about`
+            : `/${this.props.langKey}/portfolio`
+        }
+      >
+        <div className={styles.link}>
+          <span>
+            <FormattedMessage
+              id={
+                this.props.pageName === "portfolio"
+                  ? "menu.item.we.are.happy"
+                  : "check.out.appetizers"
+              }
+              values={{
+                strong: (...chunks) => <strong>{chunks}</strong>,
+              }}
+            />
+          </span>
+          <FontAwesomeIcon icon={["fas", "arrow-right"]} size="2x" />
+        </div>
+      </Link>
+    )
+  }
+
+  renderCustomFuture() {
+    if (this.props.pageName !== "about") {
+      return undefined
+    }
+
+    return (
+      <img
+        className={styles.customFuture}
+        src={"/images/customfuture.svg"}
+        alt="customfuture"
+      />
     )
   }
 }
