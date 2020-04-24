eb5c3b61782f38ef2e660a156fcce60a7881076e
peterpeterparker <david.dalbusco@outlook.com>
2020-04-24 17:42:49 +0200

    fix: icons use on each pages

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -14,6 +14,23 @@ import { getCurrentLangKey } from "ptz-i18n"
 import Navigation from "../navigation/navigation"
 import Footer from "../footer/footer"
 
+import { library, config } from "@fortawesome/fontawesome-svg-core"
+
+import { faPlus } from "@fortawesome/free-solid-svg-icons"
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons"
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons"
+import { faArrowDown } from "@fortawesome/free-solid-svg-icons"
+
+// https://github.com/FortAwesome/react-fontawesome/issues/134#issuecomment-471940596
+import "@fortawesome/fontawesome-svg-core/styles.css"
+
+config.autoAddCss = false
+
+library.add(faPlus)
+library.add(faAngleDown)
+library.add(faArrowRight)
+library.add(faArrowDown)
+
 const Layout = ({
   children,
   location,
